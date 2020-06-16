437529151265278e7f7236a5da646372b5838ed9
jinkyungpark <kyung5896@gmail.com>
2020-06-16 23:17:47 +0900

    scrapy added

--- scrapys/scrapys/spiders/test.py
+++ scrapys/scrapys/spiders/test.py
@@ -9,5 +9,5 @@ class TestSpider(scrapy.Spider):
 
     def parse(self, response):
         print('dir', dir(response))   # 사용할 수 있는 함수 정보
-        print('status', response.status)  # 상태
+        print('status', response.status)  # 상태코드
         print('text', response.body)  # body 정보
---
+++ scrapys02/result.jl
@@ -0,0 +1,10 @@
+{"number": 0, "title": "Price Gouging or Economics at Work: Price Intelligence to Track Consumer Sentiment"}
+{"number": 1, "title": "A Practical Guide to Web Data QA Part III: Holistic Data Validation Techniques"}
+{"number": 2, "title": "Product Reviews API (Beta): Extract Product Reviews at Scale"}
+{"number": 3, "title": "Custom crawling & News API: designing a web scraping solution"}
+{"number": 4, "title": "Vehicle API (Beta): Extract Automotive Data at Scale"}
+{"number": 5, "title": "A Practical Guide to Web Data Extraction QA Part II: Common validation pitfalls"}
+{"number": 6, "title": "Transitioning to Remote Working as a Company"}
+{"number": 7, "title": "A Practical Guide to Web Data QA Part I: Validation Techniques"}
+{"number": 8, "title": "COVID-19: Handling the Situation as a Fully Remote Company"}
+{"number": 9, "title": "Extracting clean article HTML with News API"}
