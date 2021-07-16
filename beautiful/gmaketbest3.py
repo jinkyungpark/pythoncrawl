f3906a6ed78ac075cd440e473f0fe9d2fa0f6095
jinkyungpark <kyung5896@gmail.com>
2021-07-16 14:55:53 +0900

    Update gmaketbest3.py

--- beautiful/gmaketbest3.py
+++ beautiful/gmaketbest3.py
@@ -1,55 +1,64 @@
 # divided and conquer : 분할하고 정복
 import requests
 from bs4 import BeautifulSoup
-import openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import Font,Alignment
 
-# 엑셀 파일 저장 준비
-gmarket_best100 = openpyxl.Workbook()
+# 엑셀 저장 준비
+wb = Workbook()
 
-sheet = gmarket_best100.active
-sheet.title = '베스트100'
-sheet.column_dimensions['B'].width = 30
-sheet.column_dimensions['C'].width = 80
-sheet.column_dimensions['D'].width = 75
-sheet.column_dimensions['E'].width = 20
-sheet.append(['번호', '회사명', '제품명', '상세정보 url', '가격'])
-##################################
+# 기본시트 활성화
+sheet1 = wb.active
+sheet1.title = "컴퓨터전자베스트100"
 
+# 컬럼 너비 조절
+sheet1.column_dimensions['B'].width = 30
+sheet1.column_dimensions['C'].width = 80
+sheet1.column_dimensions['D'].width = 75
+sheet1.column_dimensions['E'].width = 20
 
-# 크롤링 데이터를 다시 크롤링
-# g마켓 => BEST => 컴퓨터/전자 크롤링
+# 제목 행 추가
+sheet1.append(['번호','회사명','제품명','상세정보url','가격'])
 
-# 경로
-url = 'http://corners.gmarket.co.kr/Bestsellers?viewType=G&groupCode=G06'
+url = "http://corners.gmarket.co.kr/Bestsellers?viewType=G&groupCode=G06"
 
-res = requests.get(url)
+# 제품명, 가격
 
-soup = BeautifulSoup(res.content, 'html.parser')
-# print(soup)
+response = requests.get(url)
+soup = BeautifulSoup(response.content,'html.parser')
 
-lis = soup.select('div.best-list')
-lis_items = lis[1]
-# print(lis_items)
+# best-list 클래스명을 가진 요소가 2개 추출
+best_list = soup.select("div.best-list")[1]
+# 제품목록 추출
+best_list_li = best_list.select("ul > li")
+# print(best_list_li)
 
-items = lis_items.select('ul > li')
-for i, item in enumerate(items, 1):
-    title = item.select_one('a.itemname')
-    price = item.select_one('div.s-price > strong')
-    # a 태그에 들어있는 url 가져오기
-    # print(title['href'])
-    product_url = requests.get(title['href'])
-    company_res = BeautifulSoup(product_url.content, 'html.parser')
-    name = company_res.select_one(
-        'div.item-topinfo > div.item-topinfo_headline > p > span > a')
-    # print(name.get_text())
-    # print(i,name.get_text(), title.get_text(),price.get_text())
+for idx,item in enumerate(best_list_li,1):
+    # 제품명 추출
+    product_name=item.select_one("a.itemname")
+    # 제품 가격 추출
+    product_price=item.select_one("div.s-price span")
+    
+    # url을 이용하여 회사정보 추출
+    product_company_response = requests.get(product_name['href'])
+    soup = BeautifulSoup(product_company_response.content,'html.parser')
 
-    # 엑셀파일로 저장하기
-    sheet.append([i, name.get_text(), title.get_text(),
-                  title['href'], price.get_text()])
+    # 회사명 추출
+    product_company = soup.select_one("span.text")
+
+    # AttributeError: 'NoneType' object has no attribute 'text'
+    if product_company:
+        product_company = product_company.text
+    else:
+        product_company = soup.select_one('span.text__seller > a').text
+
+    print(idx,product_company,product_name.text,product_price.text,product_name['href'])
+    
+    # ['번호','회사명','제품명','상세정보url','가격']
+    sheet1.append([idx,product_company,product_name.text,product_name['href'],product_price.text])
 
     # 주소가 들어 있는 부분 하이퍼링크 걸어주기
-    sheet.cell(row=i+1, column=4).hyperlink = title['href']
+    sheet.cell(row=i+1, column=4).hyperlink = product_name['href']
 
 # 셀 선택하기
 cell_A1 = sheet['A1']
