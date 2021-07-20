869fab27f1c93049f8afd49f2176ab1a08099fbc
jinkyungpark <kyung5896@gmail.com>
2021-07-20 16:51:08 +0900

    Update daum.py

--- regex/daum.py
+++ regex/daum.py
@@ -6,4 +6,9 @@ response = requests.get("https://www.daum.net/")
 
 soup = BeautifulSoup(response.content,"html.parser")
 
+# h로 시작하는 태그 찾기
 print(soup.find_all(re.compile("h\d")))
+
+# 이미지 파일만 찾아오기
+print(soup.find_all('img',attrs={'src': re.compile('.+\.jpg')}))
+print(soup.find_all('img',attrs={'src': re.compile('.+\.(jpg|png)')}))
